5a71c8c3b06066e22689729d142b4436e92c4a14
HeWhoHoldsAKey <clintnichols2003@gmaail.com>
2023-10-28 01:37:51 -0500

    0.0.0.1

---
+++ app/src/main/java/examples/App.java
@@ -0,0 +1,12 @@
+/*
+ * This file is going to be used for testing for now.
+ * I may make different examples in here but for now it will serve as a testing file of sorts.
+ * Hell maybe ill add documentation for everything in here. Never know.
+ */
+package examples;
+
+public class App {
+
+    public static void main(String[] args) {
+    }
+}
